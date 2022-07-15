86d97cd0db9031d6c0981cced6d9f0558b523832
aleksmazur <markovasasha94@gmail.com>
2022-07-15 14:47:26 +0300

    feat: final

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const btnContact = document.querySelector('.btn_contact');
 const contactFormTitle = document.querySelector('.contact-form_title');
 const form = document.querySelector('.form');
 const feedback = document.querySelector('.feedback');
+const next = document.querySelector('.right_arrow');
+const prev = document.querySelector('.left_arrow');
+const slideWindow = document.querySelector('.slider_inner');
 
-
-function openMenu() {
+function toggleMenu() {
     menu.classList.toggle('adaptive-menu-active');
     overlay.classList.toggle('overlay-active');
-    // if (!humburger.classList.contains('humburger-active')) {
-    //     humburger.classList.add('humburger-active');
-    // }
 }
 
 function thankYou() {
@@ -23,25 +22,44 @@ function thankYou() {
     feedback.classList.add('feedback-active');
 }
 
-humburger.addEventListener('click', openMenu);
+humburger.addEventListener('click', toggleMenu);
 navList.forEach(item => {
-    item.addEventListener('click', openMenu);
+    item.addEventListener('click', toggleMenu);
 });
 btnContact.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event.target);
     window.scrollTo(0, document.documentElement.scrollHeight);
 });
-btnSend.addEventListener('click', (event) => {
+
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     thankYou();
 });
+
 menu.addEventListener('click', (event) => {
     console.log(event.target)
     if(event.target.classList.contains('btn_contact')) {
-        openMenu();
+        toggleMenu();
         window.scrollTo(0, document.documentElement.scrollHeight);
     } else if (event.target.classList.contains('header_container') || event.target.classList.contains('close')) {
-        openMenu();
+        toggleMenu();
     }
+});
+
+let offset = 0;
+next.addEventListener('click', () => {
+    offset += 285;
+    if (offset > 855) {
+        offset = 0;
+    }
+    slideWindow.style.left = -offset + 'px';
+});
+
+prev.addEventListener('click', () => {
+    offset -= 285;
+    if (offset < 0) {
+        offset = 855;
+    }
+    slideWindow.style.left = -offset + 'px';
 })
